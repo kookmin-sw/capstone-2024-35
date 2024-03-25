0dedbd98f58e2bb15d81f41939f951a83add094c
max <inews2432@gmail.com>
2024-03-26 02:36:09 +0900

    프론트 로그인 구현

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -20,6 +20,15 @@ function login() {
         body: JSON.stringify(req)
     })
     .then((res) => (res.json()))
-    .then((res) => console.log(res));
+    .then((res) => {
+        if (res.sucess) {
+            location.href = "/";
+        }else {
+            alert(res.msg);
+        }
+    })
+    .catch((err) => {
+        console.err(new Error("로그인 중 에러 발생"));
+    });
 
 }
